cc471de05bcc9dd5800cc2dcce72ebff99baa514
vlalin <38396945+vlalin@users.noreply.github.com>
2019-03-08 16:11:53 +0200

    Copying the browser history file when closing the browser (Google Chrome)

--- Copi/Program.cs
+++ Copi/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Copi
 {
@@ -6,7 +11,79 @@ namespace Copi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isOpen = false;
+            Process p = null;
+            do
+            {
+                if (isOpen == false)
+                {
+                    p = Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + @"\Google\Chrome\Application\chrome.exe");
+                    isOpen = true;
+                }
+                if (p.HasExited)
+                {
+                    Count();
+                    Thread.Sleep(123);
+                    return;
+                }
+
+
+            } while (true);
+
+            // WriteFile(newPath, ms);
+
+            // альтернатива с помощью класса File
+            // File.Copy(path, newPath, true);
+
         }
+        
+        public static async void Count()
+        {
+            try
+            {
+                string appData = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
+                appData += @"\Google\Chrome\User Data\Default";
+
+
+                string path = appData;
+                string newPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+                FileInfo fileInf = new FileInfo(path);
+
+                string[] files = Directory.GetFiles(appData);
+                foreach (var f in files)
+                {
+
+                    if (Path.GetFileName(f).ToString().Equals("History"))
+                    {
+                        appData = Path.GetFullPath(f);
+
+                    }
+                }
+
+                FileStream ms = new FileStream(appData, FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
+                Console.WriteLine(ms.Seek(0, SeekOrigin.Begin));
+                byte[] buffer = new byte[ms.Length];
+                int len;
+                while ((len = await ms.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+
+                }
+                Console.WriteLine(ms.Length);
+                Console.WriteLine(buffer.Length);
+
+                File.WriteAllBytes("history", buffer);
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("asd");
+            }
+
+
+        }
+
+
     }
+
 }
